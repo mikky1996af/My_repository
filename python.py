8ebea76c2c1ebd12f00f5996466fc872ae4896b5
Mikky <mikky1996af@mail.ru>
2022-09-15 14:55:28 +0500

    my 4 commit

--- python.py
+++ python.py
@@ -2,4 +2,8 @@ n, m = list(map(int, input().split()))
 matrix = [[i for i in range(1, m + 1)] for j in range(n)]
 for i in range(9):
     print("Hello, World!")
+    
+    
+def digit(x):
+    return x ** 777 ** 0.5
  
